ba52f63723e261dcd3c22c5e443e22ae92812f94
gzj1000xp <gzj1000xp@hotmail.com>
2017-07-19 17:35:42 +0800

    add gzip

--- img.py
+++ img.py
@@ -4,6 +4,7 @@
 import re
 import requests
 import os
+import tarfile
 
 class Spider:
     def savePageInfo(self, _url, _position, _regX):
@@ -28,9 +29,23 @@ class Spider:
             fp.close()
             i+=1
 
-for position_end in range (33000,33999):
-	url = 'http://www.jdlingyu.moe/' + position_end
-	position = '/home/pyadm/222/jdly/' + position_end
+
+def make_targz(output_filename, source_dir):
+	with tarfile.open(output_filename, "w:gz") as tar:
+		tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+start_from_num = 32000
+stop_at_num = 32999
+tar_name = str(start_from_num) + ".tar.gz"
+tar_path = "/home/pyadm/222/jdly" + str(start_from_num) + "/"
+#store_path="/home/pyadm/222/jdly/"
+
+for position_end in range (start_from_num,stop_at_num):
+	url = 'http://www.jdlingyu.moe/' + str(position_end)
+	position = '/home/pyadm/222/jdly32000/' + str(position_end)
 	regX = r'original="(.+?\.jpg)"'
 	spider = Spider()
 	spider.savePageInfo(url, position, regX)
+
+
+make_targz(tar_name, tar_path)
