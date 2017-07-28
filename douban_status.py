7d6b3ca4450e5956975f17b0983a68d1475cb803
gzj1000xp <gzj1000xp@hotmail.com>
2017-07-28 17:33:28 +0800

    0728proxy

--- douban_status.py
+++ douban_status.py
@@ -5,9 +5,22 @@ import os
 import requests
 import re
 import time
+import json
+import random
 
 
-def login(username, password):
+# 配置爬虫代理
+def getproxy(ip_pool_url):
+    ip_url = requests.get(ip_pool_url)
+    ip_url_bs = BeautifulSoup(ip_url.content, "html.parser")
+    ip_list = json.loads(str(ip_url_bs))
+    proxy_ip = random.choice(ip_list)[0]
+    proxy_port = random.choice(ip_list)[1]
+    proxy_add = proxy_ip + ":" + str(proxy_port)
+    return proxy_add
+
+# 登录函数
+def login(username, password, proxy_address):
     login_str = u'登录'
     data = {
         'source':'None',
@@ -17,14 +30,14 @@ def login(username, password):
     }
     login_url = 'https://www.douban.com/accounts/login'
     session = requests.session()
-    html = session.get(login_url, headers=headers).text
+    html = session.get(login_url, headers=headers, proxies=proxy_address).text
     captcha_img_pattern = r'(?<=<img id="captcha_image" src=\").*?(?=\")'
     captcha_image_url = re.search(captcha_img_pattern,html,re.S|re.M|re.I)
     if captcha_image_url is not None:
         captcha_image_url = captcha_image_url.group()
         print(captcha_image_url)
         # captcha_image = session.get(captcha_image_url).text
-        captcha_image = requests.get(captcha_image_url, headers=headers).content
+        captcha_image = requests.get(captcha_image_url, headers=headers, proxies=proxy_address).content
         document = 'login_captcha_douban.jpg'
         file_ = open(document, 'wb')
         file_.write(captcha_image)
@@ -39,17 +52,17 @@ def login(username, password):
         else:
             captcha_id = captcha_id.group()
             data['captcha-id'] = captcha_id
-    session.post(login_url, headers=headers, data=data)
+    session.post(login_url, headers=headers, data=data, proxies=proxy_address)
     print(data)
     print(session.cookies.items())
     return session
 
 # 解析网页，得到图片的URL，调用下载模块 need to login.
-def getpage(memberid, pageid, login_session, leastlink=""):
+def getpage(memberid, pageid, login_session, proxy_address, leastlink=""):
     pageurl = u"https://www.douban.com/people/%s/statuses?p=%d" % (memberid, pageid)  # 获取页面地址
     print(pageurl)
     #session = requests.session()
-    html = login_session.get(pageurl, headers=headers)
+    html = login_session.get(pageurl, headers=headers, proxies=proxy_address)
     #print(html)
     # 返回网页内容
     soup = BeautifulSoup(html.content, "html.parser")
@@ -62,7 +75,7 @@ def getpage(memberid, pageid, login_session, leastlink=""):
     if not pageurl == leastlink:
         pageid += 1
         leastlink = pageurl
-        getpage(memberid, pageid, login_session, leastlink)
+        getpage(memberid, pageid, login_session, proxy_address, leastlink)
     else:
         pageid = pageid - 1
         print("共爬到%d页" % pageid)
@@ -83,19 +96,17 @@ def downpic(img_src, memberid, picname):
     
     if not os.path.isfile(filename):
         try:
-            urllib.request.urlretrieve(img_src, filename)
+            urllib.request.urlretrieve(img_src, filename, proxies=proxy_address)
             time.sleep(1)
         except Exception:
             print(u'这张图片下载出问题了： %s' % filename)
-
 
 # 程序入口
 if __name__ == '__main__':
     page = 0
     member_id = "91886435"
-    #answer_id = "201250070"
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
-headers = {'User-Agent': user_agent, 'Referer': 'http://douban.com'}
+#user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+#headers = {'User-Agent': user_agent, 'Referer': 'http://douban.com'}
 # 准备headers
 pagenum = 0
 picnum = 0
@@ -103,14 +114,16 @@ picnum = 0
 
 username = 'yixi1993@hotmail.com'
 password = 'woshigzj'
+ip_pool_url = "http://127.0.0.1:8000/?types=0&count=50&country=国内"
+proxy_address = getproxy(ip_pool_url)
 
-login_headers = {
+headers = {
     "Host": "www.douban.com",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.6",
     "Accept-Encoding": "gzip, deflate, sdch, br",
     "Connection": "keep-alive"
 }
-login_session = login(username, password)
-#print(login_session)
-getpage(member_id, 0, login_session)
+login_session = login(username, password, proxy_address)
+getpage(member_id, 0, login_session, proxy_address)
+
